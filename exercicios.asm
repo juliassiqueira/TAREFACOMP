40d5a95ec2f8fb2f1c3eeaa3bc638616fb7a843b
juliassiqueira <s.siqueira@aluno.ifsp.edu.br>
2025-09-07 11:28:01 -0300

    Add files via upload

---
+++ exercicios.asm
@@ -0,0 +1,166 @@
+1) Sub-rotinas para zerar registradores
+
+Objetivo: Sub-rotinas para zerar A, B, X individualmente e uma função para zerar todos de uma vez.
+
+; Zera registrador A
+ZERA_A:
+    LDA #0
+    RET
+
+; Zera registrador B
+ZERA_B:
+    LDB #0
+    RET
+
+; Zera registrador X
+ZERA_X:
+    LDX #0
+    RET
+
+; Zera todos os registradores
+ZERA_TODOS:
+    CALL ZERA_A
+    CALL ZERA_B
+    CALL ZERA_X
+    RET
+
+2) Soma de duas variáveis com detecção de overflow
+
+8 bits (Ahmes/Ramses):
+
+VAR1: DB 14h
+VAR2: DB 25h
+RESULT: DB 0
+OVERFLOW: DB 0
+
+SOMA_8BITS:
+    LDA VAR1
+    ADD VAR2
+    JNC SEM_OVERFLOW  ; Se não ocorreu carry, pula
+    LDA #0FFh
+    STA OVERFLOW
+    JMP SALVAR
+SEM_OVERFLOW:
+    LDA #0
+    STA OVERFLOW
+SALVAR:
+    STA RESULT
+    RET
+
+
+16 bits (Ramses 16 bits):
+
+VAR16_1: DW 1234h
+VAR16_2: DW 2345h
+RESULT16: DW 0
+OVER16: DB 0
+
+SOMA_16BITS:
+    LDA VAR16_1      ; Carrega parte baixa
+    ADD VAR16_2
+    STA RESULT16
+    JNC CHECK_HIGH
+    LDA #0FFh
+    STA OVER16
+CHECK_HIGH:
+    LDA VAR16_1+1    ; Parte alta
+    ADC VAR16_2+1
+    STA RESULT16+1
+    RET
+
+3) Limpeza de área de memória (n posições)
+START_ADDR: DW 2000h
+N: DB 10
+
+LIMPA_MEMORIA:
+    LDX START_ADDR
+    LDA #0
+LOOP:
+    STA (X)
+    INX
+    DEC N
+    JNZ LOOP
+    RET
+
+4) Comparar três variáveis
+VAR_A: DB 10
+VAR_B: DB 20
+VAR_C: DB 15
+
+COMPARA_3:
+    LDA VAR_A
+    CMP VAR_B
+    JC A_MENOR_B
+    ; A >= B
+A_MENOR_B:
+    ; Comparar com C
+    LDA VAR_A
+    CMP VAR_C
+    JC A_MENOR_C
+    ; resto...
+    RET
+
+
+(A lógica pode ser expandida para determinar maior, menor ou igual.)
+
+5) Transformação de números (Complemento de 2 e Sinal-Magnitude)
+NUM: DB 5
+
+; Complemento de 2
+TO_COMPLEMENTO2:
+    NOT NUM
+    ADD #1
+    RET
+
+; Sinal-Magnitude (ex: se negativo, coloca MSB=1)
+TO_SINAL_MAG:
+    ; Supondo NUM é positivo/negativo na variável sinal
+    ; Para números positivos, nada muda
+    ; Para negativos, NUM = abs(NUM) | 80h
+    RET
+
+6) Escrever nome em ASCII
+NOME_POS: DW 3000h
+
+; Nome: JULIA SIQUEIRA
+NOME:
+    DB "JULIA SIQUEIRA",0
+
+ESCREVE_NOME:
+    LDX NOME_POS
+    LDY NOME
+LOOP_NOME:
+    LDA (Y)
+    BEQ FIM
+    STA (X)
+    INX
+    INY
+    JMP LOOP_NOME
+FIM:
+    RET
+
+7) Multiplicação de dois números positivos
+
+Multiplicação por soma repetida:
+
+MUL_A: DB 5
+MUL_B: DB 3
+MUL_RESULT: DB 0
+
+MULTIPLICA:
+    LDA #0
+    STA MUL_RESULT
+    LDX MUL_B
+LOOP_MUL:
+    CMP #0
+    BEQ FIM_MUL
+    LDA MUL_RESULT
+    ADD MUL_A
+    STA MUL_RESULT
+    DEC MUL_B
+    JMP LOOP_MUL
+FIM_MUL:
+    RET
+
+
+(Se usar Ramses 16 bits, faça ADC para parte alta também.)
